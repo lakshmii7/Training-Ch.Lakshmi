c3b4b3541b7d834d5a2524e84a8645e8cee4d09e
lakshmii7 <69245315+lakshmii7@users.noreply.github.com>
2020-11-02 18:30:35 +0530

    Add files via upload

---
+++ ArraylistDemo.java
@@ -0,0 +1,20 @@
+package List;
+
+import java.util.ArrayList;
+
+public class ArraylistDemo {
+ public static void main(String[] args) {
+	
+	 ArrayList<Integer> a1=new ArrayList<>();
+	  a1.add(34);
+	  a1.add(56);
+	  a1.add(90);
+	  a1.add(78);
+	  a1.add(34);
+	  a1.add(23);
+	  a1.add(12);
+	  a1.add(55);
+	  a1.add(89);
+	  System.out.println(a1);
+}
+}
---
+++ Linkedlistdemo.java
@@ -0,0 +1,24 @@
+package List;
+
+
+import java.util.LinkedList;
+
+public class Linkedlistdemo {
+	public static void main(String[] args) {
+		
+		 LinkedList<Integer> a1=new LinkedList<>();
+		  a1.add(34);
+		  a1.add(56);
+		  a1.add(90);
+		  a1.add(78);
+		  a1.add(34);
+		  a1.add(23);
+		  a1.add(12);
+		  a1.add(55);
+		  a1.add(89);
+		  a1.add(37);
+		  a1.add(46);
+		  
+		  System.out.println(a1);
+	}
+}
